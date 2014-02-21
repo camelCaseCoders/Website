2018026552d568393d1bc51abca688a9421f2bec
John Rapp <johnfarnes@hotmail.com>
2014-02-21 11:18:25 +0100

    Menuitem almost works (except on resize)

--- public/scripts/menu-item.js
+++ public/scripts/menu-item.js
@@ -1,35 +1,31 @@
 angular.module('menuItem', [])
 .directive('menuItem', function() {
-    return {
-        restrict: 'C',
-        transclude: true,
-        template: '<div class="menu-item-inner" ng-transclude></div><div class="underline-base"></div><div class="underline-overlay"></div>',
-        scope: {underline: '@'},
-        link: function(scope, element, attrs) {
-            var $elements = element.find('div');
-            window.elements = $elements;
-        	var $overlay = $($elements[2])
-        	.css('background-color', scope.underline);
+	return {
+		restrict: 'C',
+		transclude: true,
+		template: '<div class="menu-item-inner" ng-transclude></div><div class="underline-base"></div><div class="underline-overlay"></div>',
+		scope: {underline: '@'},
+		link: function(scope, element, attrs) {
+			var $elements = element.find('div');
+			var $inner = $($elements[0]),
+				$base = $($elements[1]),
+				$overlay = $($elements[2])
+					.css('background-color', scope.underline);
 
-            var $base = $($elements[1]),
-                width = element.width();
-            element.mouseover(function() {
-                // $base.animate({
-                //     width: '0px'
-                // }, 100);
-                // $base.hide();
-                $base.addClass('underline-base-hover');
-                $base.width(0);
-            })
-            .mouseout(function() {
-                // $base.animate({
-                //     width: width
-                // }, 200);
-                $base.removeClass('underline-base-hover');
-                $base.width(width);
-            });
-        }
-    };
+			width = $overlay.width();
+			element.mouseover(function() {
+				$base.addClass('underline-base-hover');
+				$base.width(0);
+			})
+			.mouseout(function() {
+				$base.removeClass('underline-base-hover');
+				$base.width(width);
+			});
+			$(window).resize(function() {
+				$base.width(width);
+			});
+		}
+	};
 });
 /*
 USAGE
